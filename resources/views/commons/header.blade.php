042d5def5268f4f0e77b695ec725138eca0954eb
EC2 Default User <ec2-user@ip-172-31-10-80.us-east-2.compute.internal>
2021-08-01 23:38:49 +0900

    login

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -7,10 +7,25 @@
             <ul class="navbar-nav mr-auto"></ul>
             
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">ログイン</a></li>
-                <li class="nav-item"><a href="{{ route('signup') }}" class="nav-link">新規登録</a></li>
+                
+                @if(Auth::check())
+                
+                <li class="nav-item">{!! link_to_route('logout', 'ログアウト', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('signup', 'マイページ', [], ['class' => 'nav-link']) !!}</li>
+                
+                @else
+                
+                <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('signup', '新規登録', [], ['class' => 'nav-link']) !!}</li>
+                
+                @endif
+                
             </ul>
         </div>
-
     </nav>
+
+    @if(Auth::check())
+        {{ Auth::user()->name }} 様
+    @endif
+
 </header>
